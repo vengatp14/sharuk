93a31b12fe5c0dcfbe22d6709a0c7ecb7c7e782e
venkatesh ponraj <vengatp14@gmail.com>
2021-04-09 17:17:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
- Center the loader 
+/*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -24,13 +23,13 @@ function linkAction(){
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
-/navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 200,
+    duration: 2000,
     reset: true
 });
 
