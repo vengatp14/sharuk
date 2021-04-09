856b3d033b1cfc4458bd5aa92818d82e3d786494
venkatesh ponraj <vengatp14@gmail.com>
2021-04-09 17:20:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,56 +29,56 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 2000,
+    duration: 20,
     reset: true
 });
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.button',{delay: 20}); 
+sr.reveal('.home__img',{delay: 40}); 
+sr.reveal('.home__social-icon',{ interval: 20}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__subtitle',{delay: 40}); 
+sr.reveal('.about__text',{delay: 40}); 
 
 /*SCROLL WORK*/
-sr.reveal('.w1',{delay: 200});
-sr.reveal('.w2',{delay: 200});
-sr.reveal('.work__subtitle',{delay: 400}); 
-sr.reveal('.work__text',{delay: 400});
-sr.reveal('.button1',{delay: 200});
-sr.reveal('.button1',{delay: 200});
-sr.reveal('.button1',{delay: 200});
+sr.reveal('.w1',{delay: 20});
+sr.reveal('.w2',{delay: 20});
+sr.reveal('.work__subtitle',{delay: 40}); 
+sr.reveal('.work__text',{delay: 40});
+sr.reveal('.button1',{delay: 20});
+sr.reveal('.button1',{delay: 20});
+sr.reveal('.button1',{delay: 20});
 
 /*SCROLL QUALIFICATIONS*/
-sr.reveal('.qualif__icon',{ delay: 400}); 
-sr.reveal('.qualif__subtitle',{delay: 400}); 
-sr.reveal('.qualif__text',{delay: 400}); 
-sr.reveal('.button2',{delay: 200}); 
+sr.reveal('.qualif__icon',{ delay: 40}); 
+sr.reveal('.qualif__subtitle',{delay: 40}); 
+sr.reveal('.qualif__text',{delay: 40}); 
+sr.reveal('.button2',{delay: 20}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skill_cont',{delay: 600});  
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skill_cont',{delay: 60});  
+sr.reveal('.skills__data',{interval: 20}); 
+sr.reveal('.skills__img',{delay: 60});
 
 /*SCROLL PROJECTS*/
-sr.reveal('.pro_img',{ delay: 400}); 
-sr.reveal('.pro__subtitle',{delay: 400}); 
-sr.reveal('.pro__text',{delay: 400}); 
-sr.reveal('.ip1',{delay: 400}); 
-sr.reveal('.button3',{delay: 200}); 
+sr.reveal('.pro_img',{ delay: 40}); 
+sr.reveal('.pro__subtitle',{delay: 40}); 
+sr.reveal('.pro__text',{delay: 40}); 
+sr.reveal('.ip1',{delay: 40}); 
+sr.reveal('.button3',{delay: 20}); 
 
 /*SCROLL ACHIEVEMENTS*/
-sr.reveal('.achieve1',{ delay: 400}); 
-sr.reveal('.achieve__title',{delay: 400}); 
-sr.reveal('.button4',{delay: 400}); 
-sr.reveal('.ip1',{delay: 400}); 
-sr.reveal('.button3',{delay: 200}); 
+sr.reveal('.achieve1',{ delay: 40}); 
+sr.reveal('.achieve__title',{delay: 40}); 
+sr.reveal('.button4',{delay: 40}); 
+sr.reveal('.ip1',{delay: 40}); 
+sr.reveal('.button3',{delay: 20}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal('.contact__input',{interval: 20}); 
 
